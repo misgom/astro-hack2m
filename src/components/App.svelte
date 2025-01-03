f50c99f573f1df44f14e3e52e544af0520f29e89
misgom <misgomdev@proton.me>
2025-01-03 14:20:53 +0100

    init astro-svelte-tailwind with LLM API call

---
+++ src/components/App.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { Alert } from "flowbite-svelte"
+</script>
+
+<Alert>
+    <span class="font-medium">Ups!</span>
+    Algo salió mal
+</Alert>
---
+++ src/components/Chat.svelte
@@ -0,0 +1,49 @@
+<script>
+    import { Label, Input, Spinner } from "flowbite-svelte";
+
+    let answer = ''
+    let loading = false
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        loading = true
+        const question = event.target.question.value
+
+        const res = await fetch("http://localhost:8000/question", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ question}),
+        })
+
+        if (!res.ok) {
+            loading = false
+            return
+        }
+        const apiAnswer = await res.json()
+        answer = apiAnswer.response[0].generated_text[2].content
+        loading = false
+    }
+</script>
+
+<form class="mt-8" on:submit={handleSubmit}>
+     <div class="mb-6">
+        <Label for="question" class="block mb-2">Pregunta</Label>
+        <Input id="question" required placeholder="Intenta obtener la bandera!"></Input>
+     </div>
+</form>
+
+{#if loading}
+    <div class="flex justify-center items-center flex-col gap-y-2">
+        <Spinner />
+        <p class="font-medium text-white">Generando respuesta...</p>
+    </div>
+{/if}
+
+{#if answer}
+    <div class="mt-8">
+        <p class="font-medium text-white">Respuesta:</p>
+        <p class="font-medium text-white">{answer}</p>
+    </div>
+{/if}
